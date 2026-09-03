71a29d36876dab47b41d0bdedcf5a821126431fd
David1722ui <ainabukaki@gmail.com>
2026-09-03 16:19:27 +0500

    Add files via upload

---
+++ Base.levelD.py
@@ -0,0 +1,13 @@
+
+a= int (input("Введите число:"))
+b= int (input("Введите второе число:"))
+
+sum= a+b
+diff= a-b
+mult= a*b
+avg= (a+b) / 2
+
+print(f"Сумма:{sum}")
+print(f"Разность:{diff}")
+print(f"Произведение:{mult}")
+print(f"Среднее Арифм.:{avg}")
